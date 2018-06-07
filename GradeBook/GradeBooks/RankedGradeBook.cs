f2b2c7b8803437c1aadf76c928c940602d0a6865
savvas pamboukas <savvaspamboukas@hotmail.com>
2018-06-08 01:21:09 +0300

    second

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook
+    class RankedGradeBook :BaseGradeBook
     {
+        public RankedGradeBook(string name) :base (name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
     }
 }
